ee3c9ba7b62c6c86b2e7812342c9b4a6db50fa25
d-vega <15893653+d-vega@users.noreply.github.com>
2019-03-09 23:20:32 -0500

    Parts 1 - 3 completed

--- conversions.py
+++ conversions.py
@@ -5,11 +5,35 @@
 
 def convertCelsiusToKelvin(celsius):
     """Convert Celsius to Kelvin"""
-    kelvin = 0.0
-    return kelvin
+    to_kelvin = float(celsius) + 273.15
+    return to_kelvin
 
 
 def convertCelsiusToFahrenheit(celsius):
     """Convert Celsius to Fahrenheit"""
-    fahrenheit = 0/0
-    return fahrenheit
+    to_fahrenheit = float(celsius) * 9 / 5 + 32
+    return to_fahrenheit
+
+
+def convertFahrenheitToCelsius(fahrenheit):
+    """Convert Fahrenheit to Celsius"""
+    to_celsius = (float(fahrenheit) - 32) * 5 / 9
+    return to_celsius
+
+
+def convertFahrenheitToKelvin(fahrenheit):
+    """Convert Fahrenheit to Kelvin"""
+    to_kelvin = (float(fahrenheit) + 459.67) * 5 / 9
+    return to_kelvin
+
+
+def convertKelvinToFahrenheit(kelvin):
+    """Convert Kelvin to Fahrenheit"""
+    to_fahrenheit = float(kelvin) * 9 / 5 - 459.67
+    return to_fahrenheit
+
+
+def convertKelvinToCelsius(kelvin):
+    """Convert Kelvin to Celsius"""
+    to_celsius = kelvin - 273.15
+    return to_celsius
--- tests.py
+++ tests.py
@@ -5,31 +5,63 @@
 
 import conversions
 import unittest
+import random
 
 
 class conversionTests(unittest.TestCase):
     """Class for testing conversion functionality"""
-    celsius = range(1, 5)
-    kelvin = 278.15
-    fahrenheit = 32.0
+    nums = random.sample(range(1, 100), 5)
+    fahrenheit = random.sample(range(1, 100), 5)
     #to_celsius_from_kel = kelvin - 273.15
     #to_celsius_from_fh = (fahrenheit - 32) * 5/9
 
 
     def testConvertCelsiusToKelvin(self):
-        """convertCelsiusToKelvin gives correct Kelvin conversion"""
-        for val in celsius:
-            to_kelvin = celsius + 273.15
-            result = conversions.convertCelsiusToKelvin(val)
+        """convertCelsiusToKelvin returns correct Kelvin conversion"""
+        for celsius in self.nums:
+            to_kelvin = float(celsius) + 273.15
+            result = conversions.convertCelsiusToKelvin(celsius)
             self.assertEqual(to_kelvin, result)
 
 
     def testConvertCelsiusToFahrenheit(self):
-        """convertCelsiusToFahrenheit gives correct Fahrenheit conversion"""
-        for val in celsius:
-            to_fahrenheit = celsius * (9 / 5) + 32
-            result = conversions.convertCelsiusToFahrenheit(val)
+        """convertCelsiusToFahrenheit returns correct Fahrenheit conversion"""
+        for celsius in self.nums:
+            to_fahrenheit = float(celsius) * 9 / 5 + 32
+            result = conversions.convertCelsiusToFahrenheit(celsius)
             self.assertEqual(to_fahrenheit, result)
+
+
+    def testConvertFahrenheitToCelsius(self):
+        """convertFahrenheitToCelsius returns correct Celsius conversion"""
+        for fahrenheit in self.nums:
+            to_celsius = (float(fahrenheit) - 32) * 5 / 9
+            result = conversions.convertFahrenheitToCelsius(fahrenheit)
+            self.assertEqual(to_celsius, result)
+
+
+    def testConvertFahrenheitToKelvin(self):
+        """convertFahrenheitToKelvin returns correct Kelvin conversion"""
+        for fahrenheit in self.nums:
+            to_kelvin = (float(fahrenheit) + 459.67) * 5 / 9
+            result = conversions.convertFahrenheitToKelvin(fahrenheit)
+            self.assertEqual(to_kelvin, result)
+
+
+    def testConvertKelvinToFahrenheit(self):
+        """convertKelvinToFahrenheit returns correct Fahrenheit conversion"""
+        for kelvin in self.nums:
+            to_fahrenheit = float(kelvin) * 9 / 5 - 459.67
+            result = conversions.convertKelvinToFahrenheit(kelvin)
+            self.assertEqual(to_fahrenheit, result)
+
+
+    def testConvertKelvinToCelsius(self):
+        """convertKelvinToCelsius returns correct Celsius conversion"""
+        for kelvin in self.nums:
+            to_celsius = float(kelvin) - 273.15
+            result = conversions.convertKelvinToCelsius(kelvin)
+            self.assertEqual(to_celsius, result)
 
 
 if __name__ == '__main__':
