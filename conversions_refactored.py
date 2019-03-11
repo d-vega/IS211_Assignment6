a766062668945247b24dc69b53e36eed0c501c7d
Diandra <diandrav@DESKTOP-KDJAQVJ.localdomain>
2019-03-11 00:05:23 -0400

    Part 4 completed

--- conversions_refactored.py
+++ conversions_refactored.py
@@ -1,3 +1,73 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 """Refactored version of conversions.py for IS211 Assignment 6"""
+
+
+class ConversionNotPossible(Exception):
+    """Custom class for conversion errors"""
+    pass
+
+def convert(fromUnit, toUnit, value):
+    """Docstring 1"""
+
+
+    ## Convert Temperatures ##
+
+
+    if fromUnit.lower() == "celsius" and toUnit.lower() == "kelvin":
+        celsius_to_kelvin = float(value) + 273.15
+        return celsius_to_kelvin
+
+    elif fromUnit.lower() == "celsius" and toUnit.lower() == "fahrenheit":
+        celsius_to_fahrenheit = float(value) * 9 / 5 + 32
+        return celsius_to_fahrenheit
+
+    elif fromUnit.lower() == "fahrenheit" and toUnit.lower() == "celsius":
+        fahrenheit_to_celsius = (float(value) - 32) * 5 / 9
+        return fahrenheit_to_celsius
+
+    elif fromUnit.lower() == "fahrenheit" and toUnit.lower() == "kelvin":
+        fahrenheit_to_kelvin = (float(value) + 459.67) * 5 / 9
+        return fahrenheit_to_kelvin
+
+    elif fromUnit.lower() == "kelvin" and toUnit.lower() == "fahrenheit":
+        kelvin_to_fahrenheit = float(value) * 9 / 5 - 459.67
+        return kelvin_to_fahrenheit
+
+    elif fromUnit.lower() == "kelvin" and toUnit.lower() == "celsius":
+        kelvin_to_celsius = float(value) - 273.15
+        return kelvin_to_celsius
+
+
+    ## Convert Distance ##
+
+
+    elif fromUnit.lower() == "yards" and toUnit.lower() == "miles":
+        yards_to_miles = float(value) / 1760
+        return yards_to_miles
+
+    elif fromUnit.lower() == "yards" and toUnit.lower() == "meters":
+        yards_to_meters = float(value) / 1.094
+        return yards_to_meters
+
+    elif fromUnit.lower() == "meters" and toUnit.lower() == "yards":
+        meters_to_yards = float(value) * 1.094
+        return meters_to_yards
+
+    elif fromUnit.lower() == "meters" and toUnit.lower() == "miles":
+        meters_to_miles = float(value) / 1609.344
+        return meters_to_miles
+
+    elif fromUnit.lower() == "miles" and toUnit.lower() == "yards":
+        miles_to_yards = float(value) * 1760
+        return miles_to_yards
+
+    elif fromUnit.lower() == "miles" and toUnit.lower() == "meters":
+        miles_to_meters = float(value) * 1609.344
+        return miles_to_meters
+
+    elif fromUnit.lower() == toUnit.lower():
+        return value
+
+    else:
+        raise ConversionNotPossible("Invalid units used")
--- tests.py
+++ tests.py
@@ -4,16 +4,14 @@
 
 
 import conversions
+import conversions_refactored
 import unittest
 import random
 
 
 class conversionTests(unittest.TestCase):
-    """Class for testing conversion functionality"""
+    """Class for testing conversion functionality in conversions.py"""
     nums = random.sample(range(1, 100), 5)
-    fahrenheit = random.sample(range(1, 100), 5)
-    #to_celsius_from_kel = kelvin - 273.15
-    #to_celsius_from_fh = (fahrenheit - 32) * 5/9
 
 
     def testConvertCelsiusToKelvin(self):
@@ -62,6 +60,118 @@ class conversionTests(unittest.TestCase):
             to_celsius = float(kelvin) - 273.15
             result = conversions.convertKelvinToCelsius(kelvin)
             self.assertEqual(to_celsius, result)
+
+
+    def testItemEqualsSelf(self):
+        """convert() returns itself when converted from itself"""
+        testcase = ['yards',
+                    'miles',
+                    'meters',
+                    'fahrenheit',
+                    'celsius',
+                    'kelvin']
+
+        for val in testcase:
+            unit1=unit2 = val
+
+            for num in self.nums:
+                self.assertEqual(conversions_refactored.convert(unit1, unit2,
+                                                                num), num)
+
+
+class conversionsRefactoredTests(unittest.TestCase):
+    """Unit testing for conversions_refactored.py"""
+    nums = random.sample(range(1, 100), 5)
+
+
+    def testConvertTemperatures(self):
+        """convert() returns correct temperature unit conversions"""
+
+        for val in self.nums:
+            celsius_to_kelvin = float(val) + 273.15
+            celsius_to_fahrenheit = float(val) * 9 / 5 + 32
+            fahrenheit_to_celsius = (float(val) - 32) * 5 / 9
+            fahrenheit_to_kelvin = (float(val) + 459.67) * 5 / 9
+            kelvin_to_fahrenheit = float(val) * 9 / 5 - 459.67
+            kelvin_to_celsius = float(val) - 273.15
+
+
+            test1 = conversions_refactored.convert("celsius", "kelvin", val)
+            test2 = conversions_refactored.convert("celsius", "fahrenheit",
+                                                     val)
+            test3 = conversions_refactored.convert("fahrenheit", "celsius",
+                                                     val)
+            test4 = conversions_refactored.convert("fahrenheit", "kelvin",
+                                                     val)
+            test5 = conversions_refactored.convert("kelvin", "fahrenheit",
+                                                     val)
+            test6 = conversions_refactored.convert("kelvin", "celsius", val)
+
+
+            self.assertEqual(celsius_to_kelvin, test1,
+                             "Celsius to Kelvin conversion failed")
+            self.assertEqual(celsius_to_fahrenheit, test2,
+                             "Celsius to Fahrenheit conversion failed")
+            self.assertEqual(fahrenheit_to_celsius, test3,
+                             "Fahrenheit to Celsius conversion failed")
+            self.assertEqual(fahrenheit_to_kelvin, test4,
+                             "Fahrenheit to Kelvin conversion failed")
+            self.assertEqual(kelvin_to_fahrenheit, test5,
+                             "Kelvin to Fahrenheit conversion failed")
+            self.assertEqual(kelvin_to_celsius, test6,
+                             "Kelvin to Celsius conversion failed")
+
+
+    def testConvertDistance(self):
+        """convert() returns correct distance conversions"""
+
+        for val in self.nums:
+            yards_to_miles = float(val) / 1760
+            yards_to_meters = float(val) / 1.094
+            meters_to_yards = float(val) * 1.094
+            meters_to_miles = float(val) / 1609.344
+            miles_to_yards = float(val) * 1760
+            miles_to_meters = float(val) * 1609.344
+
+
+            test1 = conversions_refactored.convert("yards", "miles", val)
+            test2 = conversions_refactored.convert("yards", "meters", val)
+            test3 = conversions_refactored.convert("meters", "yards", val)
+            test4 = conversions_refactored.convert("meters", "miles", val)
+            test5 = conversions_refactored.convert("miles", "yards", val)
+            test6 = conversions_refactored.convert("miles", "meters", val)
+
+
+            self.assertEqual(yards_to_miles, test1,
+                             "Yards to Miles conversion failed")
+            self.assertEqual(yards_to_meters, test2,
+                             "Yards to Meters conversion failed")
+            self.assertEqual(meters_to_yards, test3,
+                             "Meters to Yards conversion failed")
+            self.assertEqual(meters_to_miles, test4,
+                             "Meters to Miles conversion failed")
+            self.assertEqual(miles_to_yards, test5,
+                             "Miles to Yards conversion failed")
+            self.assertEqual(miles_to_meters, test6,
+                             "Miles to Meters conversion failed")
+
+
+    def testInvalidUnits(self):
+        """convert() fails with invalid units"""
+        testcase = {'test1': 'cat',
+                    'test2': 'dog',
+                    'test3': 'bird',
+                    'test4': 'cow',
+                    'test5': 'pig'}
+
+        for key, val in testcase.iteritems():
+            unit1 = key
+            unit2 = val
+
+            for num in self.nums:
+                self.assertRaises(conversions_refactored.ConversionNotPossible,
+                                  conversions_refactored.convert, unit1, unit2,
+                                  num)
 
 
 if __name__ == '__main__':
